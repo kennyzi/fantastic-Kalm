b146eb8910097bfa6ac39f22b0874a6c6bd79385
Yosua Hoo <yosu4.hoo@gmail.com>
2018-11-01 15:04:06 +0700

    autoLayout History StackView, dynamic text

--- Kalm/Graph/HistoryDetailTableViewCell.swift
+++ Kalm/Graph/HistoryDetailTableViewCell.swift
@@ -7,12 +7,19 @@
 
 import UIKit
 
+@available(iOS 9.0, *)
 class HistoryDetailTableViewCell: UITableViewCell {
 
     
     @IBOutlet weak var durationLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
+    
+    
+    @IBOutlet weak var stackView: UIStackView!
+    
+    var largeTextConstraints : [NSLayoutConstraint] = []
+    var regularConstraints : [NSLayoutConstraint] = []
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -41,6 +48,38 @@ class HistoryDetailTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        if #available(iOS 11.0, *) {
+            let isAccessibilityCategory = traitCollection.preferredContentSizeCategory.isAccessibilityCategory
+            if isAccessibilityCategory != previousTraitCollection?.preferredContentSizeCategory.isAccessibilityCategory{
+                updateStackView()
+            }
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+    
+    
+    func updateStackView(){
+        if #available(iOS 11.0, *) {
+            if traitCollection.preferredContentSizeCategory.isAccessibilityCategory{
+                stackView.axis = UILayoutConstraintAxis.vertical
+                durationLabel.textAlignment = NSTextAlignment.left
+                timeLabel.textAlignment = NSTextAlignment.left
+                dateLabel.textAlignment = NSTextAlignment.left
+                stackView.spacing = 3
+            }else{
+                stackView.axis = UILayoutConstraintAxis.horizontal
+                durationLabel.textAlignment = NSTextAlignment.left
+                timeLabel.textAlignment = NSTextAlignment.center
+                dateLabel.textAlignment = NSTextAlignment.right
+                stackView.spacing = 0
+            }
+        } else {
+            // Fallback on earlier versions
+        }
     }
 
 }
